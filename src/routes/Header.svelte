15a62ebf963c9924da659475f4a97ac0d29fb613
jamesseeman <jamesseeman@protonmail.com>
2025-11-09 03:20:52 -0500

    replace logo

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,1 +1,19 @@
-<h1 class="text- text-5xl text-center font-bold mb-4">needlebot</h1>
+<script lang="ts">
+  import logo from "$lib/assets/logo.svg?raw";
+</script>
+
+<div id="logo" class="flex text-white justify-center mb-4">
+  {@html logo}
+</div>
+
+<style>
+  :global(#logo svg) {
+    height: 10%;
+    width: 20%;    
+
+  }
+
+  :global(#logo svg path) {
+    fill: white !important;
+  }
+</style>
