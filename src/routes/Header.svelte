081e9862e223f93ce7c910c3e38954752252df36
jamesseeman <jamesseeman@protonmail.com>
2025-11-09 14:07:54 -0500

    add github link

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
   import logo from "$lib/assets/logo.svg?raw";
+  import { Github } from "lucide-svelte";
 </script>
 
-<div id="logo" class="flex text-white justify-center mb-4">
-  {@html logo}
+<div class="relative">
+  <a
+    href="https://github.com/jamesseeman/needlebot"
+    target="_blank"
+    class="absolute top-0 right-0 p-4 text-white hover:opacity-80 transition-opacity"
+  >
+    <Github size={24} />
+  </a>
+  <div id="logo" class="flex text-white justify-center mb-4">
+    {@html logo}
+  </div>
 </div>
 
 <style>
   :global(#logo svg) {
     height: 10%;
-    width: 20%;    
-
+    width: 20%;
   }
 
   :global(#logo svg path) {
